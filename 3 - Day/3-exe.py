5da855d22059f20eb5d5ec18c48c18e3533d5780
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-07-22 19:21:36 +0530

    3 - Day : Conditional Statements

---
+++ 3 - Day/3-exe.py
@@ -0,0 +1,22 @@
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+
+if year % 4 == 0 and year % 400 == 0 or year % 100 != 0:
+    print("Leap Year")
+else:
+    print("Not Leap Year")
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap Year")
+        else:
+            print("Not Leap Year")
+    else:
+        print("Not Leap Year")
+else:
+    print("Not Leap Year")
