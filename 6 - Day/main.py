1aeeae67c9f3398b3c21103b54d8e60010775e79
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-07-25 16:57:23 +0530

    6 - Day : While Loop

---
+++ 6 - Day/main.py
@@ -0,0 +1,23 @@
+print("Hello")
+
+str_length = len("Arunesh")
+
+print(str_length)
+
+
+def fun():
+    print("Hello, Arunesh")
+
+
+fun()
+
+
+for x in range(0, 6):
+    print(x)
+
+print()
+
+x = 6
+while x > 0:
+    print(x)
+    x -= 1
