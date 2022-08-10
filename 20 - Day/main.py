547176480be97e891b8489e0f043eb9ab2b77ec3
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-08-11 03:03:32 +0530

    20.1 - Day : Screen setup & Creating snake body

---
+++ 20 - Day/main.py
@@ -0,0 +1,47 @@
+from turtle import Turtle, Screen
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+
+starting_position = [(0, 0), (-20, 0), (-40, 0)]
+
+
+for position in starting_position:
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.goto(position)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
