26568755b72aacec5d2baec093a48cf7a9f4b0fc
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-08-11 15:37:33 +0530

    20.2 - Day : Snake segments on screen

--- 20 - Day/main.py
+++ 20 - Day/main.py
@@ -1,22 +1,35 @@
 from turtle import Turtle, Screen
+import time
 
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
 
 starting_position = [(0, 0), (-20, 0), (-40, 0)]
-
+segments = []
+game_is_on = True
 
 for position in starting_position:
     new_segment = Turtle(shape="square")
     new_segment.color("white")
+    new_segment.penup()
     new_segment.goto(position)
+    segments.append(new_segment)
 
 
-
+while game_is_on:
+    screen.update()
+    time.sleep(0.3)
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(x=new_x, y=new_y)
+    segments[0].forward(20)
+    segments[0].left(90)
 
 
 
