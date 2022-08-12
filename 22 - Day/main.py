c32ec8162f14b9cce30ce578a3aa13b8081e9d84
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-08-12 19:11:23 +0530

    22.4 - Day : Ball bouncing logic

--- 22 - Day/main.py
+++ 22 - Day/main.py
@@ -29,9 +29,13 @@ screen.onkey(fun=l_paddle.go_down, key="s")
 # GAME CONTINUE
 game_is_on = True
 while game_is_on:
-    time.sleep(0.2)
+    time.sleep(0.1)
     screen.update()
     ball.move()
+
+    # detect ball on wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
 
 
 screen.exitonclick()
