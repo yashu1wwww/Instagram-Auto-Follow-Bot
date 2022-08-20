270d3c697cab8d3d28eb3978561110058ceb7d67
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-08-21 04:16:48 +0530

    28.3 - Day : Different Timer Sessions and values

--- 28 - Day/main.py
+++ 28 - Day/main.py
@@ -11,24 +11,59 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    # work_sec = WORK_MIN * 60
+    # short_break_sec = SHORT_BREAK_MIN * 60
+    # long_break_sec = LONG_BREAK_MIN * 60
+
+    work_sec = 5
+    short_break_sec = 2
+    long_break_sec = 10
+
+    reps += 1
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        timer_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        timer_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        timer_label.config(text="Work", fg=GREEN)
+
+    """     MY SOLUTION
+    if reps == 1 or reps == 3 or reps == 5 or reps == 7:
+        count_down(work_sec)
+    elif reps == 2 or reps == 4 or reps == 6:
+        count_down(short_break_sec)
+    elif reps == 8:
+        count_down(long_break_sec)
+    """
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
 
     count_min = math.floor(count / 60)
+    # count_min = count // 60
     count_sec = count % 60
+
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
