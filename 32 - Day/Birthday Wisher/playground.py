2ee039bdded7f83d52b3151524e4a878dd2ab068
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-08-23 17:20:37 +0530

    32.2 - Day : Month compare ( BUG FIX )

--- 32 - Day/Birthday Wisher/playground.py
+++ 32 - Day/Birthday Wisher/playground.py
@@ -14,13 +14,16 @@ password = "ocvsredwlcdumipb"
 
 #   get birthday person details
 data = pandas.read_csv("birthdays.csv")
+
 data_dict = data.to_dict(orient="records")
+# print(data_dict)
 
 for item in data_dict:
     birth_day_month = (item["day"], item["month"])
-    name = item["name"]
-    email = item["email"]
     if current_day_month == birth_day_month:
+        name = item["name"]
+        email = item["email"]
+
         #   create birthday letter
         with open(f"letter_templates/letter_{randint(1, 3)}.txt") as file_data:
             data = file_data.read()
