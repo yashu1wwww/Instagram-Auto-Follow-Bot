d2f22937824e2f81e4c3e5f72f248ae8180fbd28
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-08-27 07:00:35 +0530

    56 - Day : Flask static files

---
+++ 56 - Day/main/server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html", name="Arunesh kumar")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("pages/contact.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
