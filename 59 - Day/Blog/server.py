5193b63ea98ccbbe888234e4204d2c5616026304
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-08-30 02:25:52 +0530

    59 - Day : API Blog

---
+++ 59 - Day/Blog/server.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+import requests
+
+
+app = Flask(__name__)
+response = requests.get(url="https://api.npoint.io/7fb3ea579b2efa3f5500")
+response.raise_for_status()
+all_post = response.json()
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", posts=all_post)
+
+
+@app.route("/post/<int:post_id>")
+def get_post(post_id):
+    data = None
+    for post in all_post:
+        if post["id"] == post_id:
+            data = post
+    return render_template("post.html", post=data)
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
