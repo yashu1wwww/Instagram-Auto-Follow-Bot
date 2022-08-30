9b184ffed39e04ffa1b56908fa35d43f02fba85b
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-08-30 18:47:35 +0530

    61.2 - Day : WTform form class added

--- 61 - Day/Practice/main.py
+++ 61 - Day/Practice/main.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+
 
 app = Flask(__name__)
+
+
+class LoginForm(FlaskForm):
+    email = StringField(label="Email")
+    password = PasswordField(label="Password")
+    submit = SubmitField("Log In")
+
+
+app.secret_key = "any-string-you-want-just-keep-it-secret"
 
 
 @app.route("/")
@@ -10,7 +22,9 @@ def home():
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+
+    return render_template("login.html", form=form)
 
 
 if __name__ == '__main__':
