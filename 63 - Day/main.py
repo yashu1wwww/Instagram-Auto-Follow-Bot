3d35165cac5219ee65e1466d271ba63f91a67585
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-09-03 17:16:36 +0530

    63.1 - Day : initial files

---
+++ 63 - Day/main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+all_books = []
+
+
+@app.route('/')
+def home():
+    return render_template("index.html", books=all_books)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        title = request.form["title"]
+        author = request.form["author"]
+        rating = request.form["rating"]
+
+        data = {
+            "title": title,
+            "author": author,
+            "rating": rating
+        }
+
+        all_books.append(data)
+
+        print(all_books)
+
+        return redirect(url_for("home"))
+
+    return render_template("add.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
