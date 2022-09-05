d62519dd71d5352a67fcaadce8cd1ffdfce1f5e9
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-09-05 20:05:20 +0530

    63.3 - Day : Complete CRUD Operation

--- 63 - Day/main.py
+++ 63 - Day/main.py
@@ -1,49 +1,29 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
-all_books = []
 
 
-# db = sqlite3.connect("books-collections.db")
-# cursor = db.cursor()
-
-
-#   CREATE NEW TABLE
-# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
-
-# ADD DATA
-# cursor.execute("INSERT INTO books VALUES('Harry Potter', 'J. K. Rowling', '9.3')")
-# cursor.execute("INSERT OR IGNORE INTO books VALUES(2, 'Harry Potter 1', 'J. K. Rowling 1', '9.3')")
-# cursor.execute("INSERT OR IGNORE INTO books VALUES(3, 'Harry Potter 2', 'J. K. Rowling 2', '9.3')")
-# db.commit()
-
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books-collections.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///books-collections.db"
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
-    author = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
     rating = db.Column(db.Float, nullable=False)
-
-    def __repr__(self):
-        return '<Book %r>' % self.title
 
 
 db.create_all()
 
 
-x = Book(title="Harry Portter", author="Arunesh", rating=4.3)
-db.session.add(x)
-db.session.commit()
-
-
 @app.route('/')
 def home():
+    #   GET ALL BOOK
+    all_books = Book.query.all()
     return render_template("index.html", books=all_books)
 
 
@@ -54,19 +34,42 @@ def add():
         author = request.form["author"]
         rating = request.form["rating"]
 
-        data = {
-            "title": title,
-            "author": author,
-            "rating": rating
-        }
-
-        all_books.append(data)
-
-        print(all_books)
+        # create
+        new_book = Book(title=title, author=author, rating=rating)
+        db.session.add(new_book)
+        db.session.commit()
 
         return redirect(url_for("home"))
 
     return render_template("add.html")
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        rating = request.form["rating"]
+        book_id = request.form["id"]
+
+        book = Book.query.get(book_id)
+
+        book.rating = rating
+        db.session.commit()
+
+        return redirect(url_for("home"))
+
+    book_id = request.args.get("id")
+    book = Book.query.get(book_id)
+
+    return render_template("edit.html", book=book)
+
+
+@app.route("/delete")
+def delete():
+    book_id = request.args.get("id")
+    book = Book.query.get(book_id)
+    db.session.delete(book)
+    db.session.commit()
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
