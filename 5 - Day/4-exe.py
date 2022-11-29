63e10ed46908f098336d6c19529f9d4e3361e763
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2022-11-29 19:49:45 +0530

    Delete 5 - Day directory

--- 5 - Day/4-exe.py
+++
@@ -1,9 +0,0 @@
-for num in range(1, 101):
-    if num % 3 == 0 and num % 5 == 0:
-        num = "FizzBuzz"
-    elif num % 3 == 0:
-        num = "Fizz"
-    elif num % 5 == 0:
-        num = "Buzz"
-
-    print(num)
